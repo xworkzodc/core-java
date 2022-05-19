077acf13a8e7b0066180f7732b469e13cad25b3f
unknown <arjun.xworkz@gmail.com>
2022-05-19 09:24:52 +0530

    added for loop

---
+++ ForloopDemo2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ForloopDemo2{
+	public static void main(String[] args) {
+		System.out.println("Enter number");
+		Scanner in =new Scanner(System.in);
+		
+		int n = in.nextInt();
+		 int sum=0;
+		for(int i=1; i<=n; i++){
+			sum=sum+i;  // this will print adition of 0 to 10;
+			System.out.println(sum );
+		}
+	}
+}
