5a4aa1601db937fc59a7e34a3d63c95ab65cc820
Arjun bari <arjun.xworkz@gmail.com>
2022-06-06 19:35:57 +0530

    Single_inheritance example

---
+++ Opps/src/com/Oppps/Inheritance/Single_Inheritance/Animal.java
@@ -0,0 +1,8 @@
+package com.Oppps.Inheritance.Single_Inheritance;
+
+public class Animal {
+	void eat() {
+		 System.out.println("eavry animals eat");
+	 }
+}
+ 
---
+++ Opps/src/com/Oppps/Inheritance/Single_Inheritance/TestSingleInharitance.java
@@ -0,0 +1,9 @@
+package com.Oppps.Inheritance.Single_Inheritance;
+
+public class TestSingleInharitance {
+	public static void main(String [] args) {
+Dog dog=new Dog();
+dog.eat();
+dog.bark();
+}
+}
